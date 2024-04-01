7139a3c58104274bac1b2adaeb6cdbe0c5d1dc9d
Karimjon0 <161305610+Karimjon0@users.noreply.github.com>
2024-04-01 14:53:06 +0500

    Program.cs

--- Register/Program.cs
+++ Register/Program.cs
@@ -7,14 +7,12 @@ bool isContinue = true;
 do
 {
     Console.WriteLine("Log In          |          Sign Up");
+    
     Console.Write("Enter command: ");
     string command = Console.ReadLine();
     if (command.Contains("1") is true)
     {
-        bookService.ReadAllName();
-    }
-    if (command.Contains("2") is true)
-    {
+        Console.WriteLine("- - - - - Log In - - - - -");
         Registers bookInfo = new Registers();
         Console.Write("Enter first name: ");
         bookInfo.FirstName = Console.ReadLine();
@@ -25,7 +23,18 @@ do
 
         bookService.InsertName(bookInfo);
     }
+    if (command.Contains("2") is true)
+    {
+        Console.WriteLine("- - - - - Sign Up - - - - -");
+        Registers bookInfo = new Registers();
+        Console.Write("Enter first name: ");
+        bookInfo.FirstName = Console.ReadLine();
+        Console.Write("Enter last name: ");
+        bookInfo.LastName = Console.ReadLine();
+        Console.Write("Enter password: ");
+        bookInfo.Password = Console.ReadLine();
 
+        bookService.InsertName(bookInfo);
     Console.WriteLine("Will you continue?");
     string isCommand = Console.ReadLine();
     if (isCommand.ToLower().Contains("no") is true || isCommand.ToLower().Contains("yuq") is true)
@@ -33,3 +42,4 @@ do
         isContinue = false;
     }
 } while (isContinue is true);
+    }
