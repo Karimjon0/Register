6f5fff765ad263bd2774809c242dcc6603b0265c
Karimjon0 <161305610+Karimjon0@users.noreply.github.com>
2024-04-15 14:41:17 +0500

    Update Program.cs

--- Register/Program.cs
+++ Register/Program.cs
@@ -1,4 +1,4 @@
-﻿using Book.CRUD.Service;
+using Book.CRUD.Service;
 using Register.Models;
 
 IRegisterService bookService = new RegisterService();
@@ -6,40 +6,46 @@ IRegisterService bookService = new RegisterService();
 bool isContinue = true;
 do
 {
-    Console.WriteLine("Log In          |          Sign Up");
-    
-    Console.Write("Enter command: ");
-    string command = Console.ReadLine();
-    if (command.Contains("1") is true)
-    {
-        Console.WriteLine("- - - - - Log In - - - - -");
-        Registers bookInfo = new Registers();
-        Console.Write("Enter first name: ");
-        bookInfo.FirstName = Console.ReadLine();
-        Console.Write("Enter last name: ");
-        bookInfo.LastName = Console.ReadLine();
-        Console.Write("Enter password: ");
-        bookInfo.Password = Console.ReadLine();
+        Console.WriteLine("WELCOME :)");
+        Console.WriteLine("Log In          |          Sign Up");
 
-        bookService.InsertName(bookInfo);
-    }
-    if (command.Contains("2") is true)
-    {
-        Console.WriteLine("- - - - - Sign Up - - - - -");
-        Registers bookInfo = new Registers();
-        Console.Write("Enter first name: ");
-        bookInfo.FirstName = Console.ReadLine();
-        Console.Write("Enter last name: ");
-        bookInfo.LastName = Console.ReadLine();
-        Console.Write("Enter password: ");
-        bookInfo.Password = Console.ReadLine();
+        Console.Write("Enter command: ");
+        string command = Console.ReadLine();
+        if (command.Contains("1") is true)
+        {
+            Console.WriteLine("- - - - - Log In - - - - -");
+            Registers bookInfo = new Registers();
+            Console.Write("Enter username: ");
+            bookInfo.UserName = Console.ReadLine();
+            Console.Write("Enter password: ");
+            bookInfo.Password = Console.ReadLine();
 
-        bookService.InsertName(bookInfo);
-    Console.WriteLine("Will you continue?");
-    string isCommand = Console.ReadLine();
-    if (isCommand.ToLower().Contains("no") is true || isCommand.ToLower().Contains("yuq") is true)
-    {
-        isContinue = false;
-    }
-} while (isContinue is true);
-    }
+            string str = bookInfo.UserName;
+            string str2 = str.Substring(-1, -10);
+
+            if (str2 == "@gmail.com")
+            {
+            System.Console.WriteLine("Welcome");
+            }
+            else
+            {
+            Console.WriteLine("Invalid");
+            }
+
+
+
+            bookService.InsertName(bookInfo);
+
+        //karimjon@gmail.com
+        }
+        if (command.Contains("2") is true)
+        {
+            Console.WriteLine("- - - - - Sign Up - - - - -");
+            Registers bookInfo = new Registers();
+            Console.Write("Enter username: ");
+            bookInfo.UserName = Console.ReadLine();
+            Console.Write("Enter password: ");
+            bookInfo.Password = Console.ReadLine();
+        }
+}
+
