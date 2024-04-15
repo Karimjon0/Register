b58c9b93bd839456b88fe96343b0ebc73e4b786f
Karimjon0 <161305610+Karimjon0@users.noreply.github.com>
2024-04-15 14:41:59 +0500

    Update Registers.cs

--- Register/Models/Registers.cs
+++ Register/Models/Registers.cs
@@ -1,10 +1,9 @@
-﻿
+
 namespace Register.Models
 {
     public class Registers
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string UserName { get; set; }
         public string Password { get; set; }
     }
 }
